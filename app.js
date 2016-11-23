88a92acbc5a630523ad33761b5759a054df9c735
cold <hemems@163.com>
2016-11-23 19:03:07 +0800

    v8

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./public/1/index.html');
+var index = require('./routes/index');
 var users = require('./routes/users');
 
 
